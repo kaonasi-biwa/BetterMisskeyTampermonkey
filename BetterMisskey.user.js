54c72eccc8d16bf9923d8cc16a519b003a731b0c
kaonasi_biwa(加尾梨 毘和) <85387717+kaonasi-biwa@users.noreply.github.com>
2023-03-17 21:44:19 +0900

    Update BetterMisskey.user.js

--- BetterMisskey.user.js
+++ BetterMisskey.user.js
@@ -28,7 +28,7 @@ function observerFunc(){
 }
 function eventClick(event){
     console.log(event.target)
-    if(event.target.tagName != "IMG" && event.target.parentElement.tagName != "BUTTON" && event.target.tagName != "A" && event.target.tagName != "SPAN" && event.target.tagName != "LI"){
+    if(event.target.tagName != "IMG" && event.target.tagName != "BUTTON" && event.target.parentElement.tagName != "BUTTON" && event.target.tagName != "A" && event.target.tagName != "SPAN" && event.target.tagName != "LI"){
               event.currentTarget.parentElement.querySelector(`header [href^="/notes/"]`).click()
 
     }event.stopPropagation()
