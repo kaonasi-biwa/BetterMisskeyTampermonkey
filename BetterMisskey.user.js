22731bc4e215fe4880f9e7cc059ab27e8d42771c
kaonasi_biwa(加尾梨 毘和) <85387717+kaonasi-biwa@users.noreply.github.com>
2023-03-17 21:14:09 +0900

    Add Code

---
+++ BetterMisskey.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Better Misskey
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  matchにお好みのMisskeyインスタンスを入力してください
+// @author       kaonasi_biwa
+// @match        https://mi.ablaze.one/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=misskey.io
+// @grant        none
+// ==/UserScript==
+
+let observer = new MutationObserver(observerFunc)
+const setObs = ()=>{
+    if(document.querySelector("#misskey_app")){
+        observer.observe(document.querySelector("#misskey_app"),{childList: true,subtree: true})
+    }
+    else{
+        window.setTimeout(setObs,1000)
+    }
+}
+
+function observerFunc(){
+    let icons = document.querySelectorAll(`[tabindex="-1"] > :is(article,div) [href^="/@"][title]:not(.misskeyKaonasi)`)
+    for(let elem of icons){
+        if(elem.parentElement.querySelector(`header [href^="/notes/"]`) != null) elem.parentElement.onclick = eventClick
+        elem.classList.add("misskeyKaonasi")
+    }
+}
+function eventClick(event){
+    console.log(event.currentTarget)
+event.currentTarget.parentElement.querySelector(`header [href^="/notes/"]`).click()
+    event.stopPropagation()
+}
+
+(function() {
+    'use strict';
+    setObs()
+    // Your code here...
+})();
