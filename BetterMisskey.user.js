099ed808fe2b2d59015fd97ee73171bf9bf3c9c7
kaonasi_biwa(加尾梨 毘和) <85387717+kaonasi-biwa@users.noreply.github.com>
2023-07-02 22:14:40 +0900

    Version Up

--- BetterMisskey.user.js
+++ BetterMisskey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Misskey
 // @namespace    http://tampermonkey.net/
-// @version      0.1.14
+// @version      0.2.0
 // @description  include等にお好みのMisskeyインスタンスを入力して利用してください
 // @author       kaonasi_biwa
 // @homepage     https://github.com/kaonasi-biwa/BetterMisskeyTampermonkey
@@ -20,6 +20,9 @@
 // @match        *://submarin.online/*
 // ==/UserScript==
 
+let articleClick = true; //ノートクリックでTwitterのように拡大表示できるようにする
+let followIconClick = true; //フォロー・フォロワー一覧でアイコンクリックだけでプロフィール表示できるようにする
+let rnUseQuote = true; //RNを引用RNを使ったものにする
 
 let observer = new MutationObserver(observerFunc)
 const setObs = ()=>{
@@ -32,18 +35,35 @@ const setObs = ()=>{
 }
 
 function observerFunc(){
-    let icons = document.querySelectorAll(`[tabindex="-1"] > :is(article,div) [href^="/@"][title]:not(.misskeyKaonasi)`)
+    observer.disconnect();
+    if(articleClick){
+        let icons = document.querySelectorAll(`[tabindex="-1"] > :is(article,div) [href^="/@"][title]:not(.misskeyKaonasi)`)
     for(let elem of icons){
         if(elem.parentElement.querySelector(`header [href^="/notes/"]`) != null) elem.parentElement.onclick = eventClick
         elem.classList.add("misskeyKaonasi")
     }
-    if(location.host != "submarin.online"){
+    }
+    if(followIconClick && location.host != "submarin.online"){
       let followIcons = document.querySelectorAll(":is(.avatar,span[title]):not(.misskeyKaonasi):not(a > *)")
       for(let elem of followIcons){
       elem.onclick = avatarClick
         elem.classList.add("misskeyKaonasi")
       }
     }
+    if(rnUseQuote){
+      let rnIcon = document.querySelectorAll(`[role="menuitem"][tabindex="0"]:not(.misskeyKaonasi) > .ti-fw.xh8pZ.ti.ti-repeat`)
+      for(let elem of rnIcon){
+          let base = elem.parentElement.parentElement
+          elem.parentElement.outerHTML = elem.parentElement.outerHTML
+          base.children[0].classList.add("misskeyKaonasi")
+          base.children[0].addEventListener(
+              'click',
+              rnClick,
+              true
+          )
+      }
+    }
+    observer.observe(document.querySelector("#misskey_app,#app"),{childList: true,subtree: true})
 }
 function eventClick(event){
     if(event.target.tagName == "ARTICLE" || event.target.tagName == "DIV" || event.target.tagName == "FOOTER" || event.target.tagName == "HEADER"){
@@ -60,6 +80,13 @@ function avatarClick(event){
 (event.currentTarget.parentElement.querySelector("a.name") ?? event.currentTarget.parentElement.querySelector(`a[href^="/@"]`))?.click()
 }
 
+function rnClick(event){
+  event.currentTarget.nextElementSibling.click()
+    window.setTimeout(() => document.querySelector(`[data-cy-open-post-form-submit=""]`).click(),100)
+  event.stopPropagation()
+    event.stopImmediatePropagation()
+    event.preventDefault()
+}
 (function() {
     'use strict';
     setObs()
