5ab2e79ff04591430a97efff0005501512c24091
kaonasi_biwa(加尾梨 毘和) <85387717+kaonasi-biwa@users.noreply.github.com>
2025-11-15 17:33:22 +0900

    現在のMisskey向けに修正・コードを若干整形

--- BetterMisskey.user.js
+++ BetterMisskey.user.js
@@ -22,73 +22,78 @@
 
 let articleClick = true; //ノートクリックでTwitterのように拡大表示できるようにする
 let followIconClick = true; //フォロー・フォロワー一覧でアイコンクリックだけでプロフィール表示できるようにする
-let rnUseQuote = true; //RNを引用RNを使ったものにする
+let rnUseQuote = false; //RNを引用RNを使ったものにする
 
 let observer = new MutationObserver(observerFunc)
-const setObs = ()=>{
-    if(document.querySelector("#misskey_app,#app")){
-        observer.observe(document.querySelector("#misskey_app,#app"),{childList: true,subtree: true})
-    }
-    else{
-        window.setTimeout(setObs,1000)
-    }
-}
+let observerRoot = document.querySelector("#misskey_app,#app")
+const observerConfig = { childList: true,subtree: true }
 
-function observerFunc(){
+function observerFunc() {
     observer.disconnect();
-    if(articleClick){
-        let icons = document.querySelectorAll(`[tabindex="-1"] > :is(article,div) [href^="/@"][title]:not(.misskeyKaonasi)`)
-    for(let elem of icons){
-        if(elem.parentElement.querySelector(`header [href^="/notes/"]`) != null) elem.parentElement.onclick = eventClick
-        elem.classList.add("misskeyKaonasi")
+    if(articleClick) {
+        let icons = document.querySelectorAll(`[tabindex] > :is(article,div) [href^="/@"][title]:not(.misskeyKaonasi)`)
+        for(let elem of icons) {
+            if(elem.parentElement.querySelector(`header [href^="/notes/"]`) != null)
+                elem.parentElement.onclick = eventClick
+            elem.classList.add("misskeyKaonasi")
+        }
     }
+    if(followIconClick && location.host != "submarin.online") {
+        let followIcons = document.querySelectorAll(":is(.avatar,span[title]):not(.misskeyKaonasi):not(a > *)")
+        for(let elem of followIcons) {
+            elem.onclick = avatarClick
+            elem.classList.add("misskeyKaonasi")
+        }
     }
-    if(followIconClick && location.host != "submarin.online"){
-      let followIcons = document.querySelectorAll(":is(.avatar,span[title]):not(.misskeyKaonasi):not(a > *)")
-      for(let elem of followIcons){
-      elem.onclick = avatarClick
-        elem.classList.add("misskeyKaonasi")
-      }
+    if(rnUseQuote) {
+        let rnIcon = document.querySelectorAll(`[role="menuitem"][tabindex="0"]:not(.misskeyKaonasi) > .ti-fw.xh8pZ.ti.ti-repeat`)
+        for(let elem of rnIcon) {
+            let base = elem.parentElement.parentElement
+            elem.parentElement.outerHTML = elem.parentElement.outerHTML
+            base.children[0].classList.add("misskeyKaonasi")
+            base.children[0].addEventListener(
+                'click',
+                rnClick,
+                true
+            )
+        }
     }
-    if(rnUseQuote){
-      let rnIcon = document.querySelectorAll(`[role="menuitem"][tabindex="0"]:not(.misskeyKaonasi) > .ti-fw.xh8pZ.ti.ti-repeat`)
-      for(let elem of rnIcon){
-          let base = elem.parentElement.parentElement
-          elem.parentElement.outerHTML = elem.parentElement.outerHTML
-          base.children[0].classList.add("misskeyKaonasi")
-          base.children[0].addEventListener(
-              'click',
-              rnClick,
-              true
-          )
-      }
-    }
-    observer.observe(document.querySelector("#misskey_app,#app"),{childList: true,subtree: true})
+    observer.observe(observerRoot, observerConfig)
 }
 function eventClick(event){
     if(event.target.tagName == "ARTICLE" || event.target.tagName == "DIV" || event.target.tagName == "FOOTER" || event.target.tagName == "HEADER"){
-        event.currentTarget.parentElement.querySelector(`header [href^="/notes/"]`).click()
+        event.currentTarget.querySelector(`header [href^="/notes/"]`).click()
     }
     event.stopPropagation()
-}
-
-function eventCancel(event){
-    event.stopPropagation()
-}
-
-function avatarClick(event){
-(event.currentTarget.parentElement.querySelector("a.name") ?? event.currentTarget.parentElement.querySelector(`a[href^="/@"]`))?.click()
-}
-
-function rnClick(event){
-  event.currentTarget.nextElementSibling.click()
-    window.setTimeout(() => document.querySelector(`[data-cy-open-post-form-submit=""]`).click(),100)
-  event.stopPropagation()
     event.stopImmediatePropagation()
     event.preventDefault()
 }
+
+function avatarClick(event){
+    (event.currentTarget.parentElement.querySelector("a.name") ?? event.currentTarget.parentElement.querySelector(`a[href^="/@"]`))?.click()
+}
+
+function rnClick(event){
+    event.currentTarget.nextElementSibling.click()
+    window.setTimeout(() => document.querySelector(`[data-cy-open-post-form-submit=""]`).click(),100)
+    event.stopPropagation()
+    event.stopImmediatePropagation()
+    event.preventDefault()
+}
+
+const setObs = () = >{
+    if(!observerRoot && document.querySelector("#misskey_app,#app")){
+        observerRoot = document.querySelector("#misskey_app,#app")
+    }
+    if(observerRoot){
+        observer.observe(observerRoot, observerConfig)
+    }
+    else{
+        window.setTimeout(setObs, 1000)
+    }
+}
+
 (function() {
     'use strict';
     setObs()
-    // Your code here...
 })();
